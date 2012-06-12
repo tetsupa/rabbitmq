e6a7ed5d985b43c61a4530d1d852f102b5ce73f4
Tetsuharu Kohkaki <tetsupa0427@gmail.com>
2012-06-12 22:29:12 +0900

    hello subscribe

---
+++ hello.rb
@@ -0,0 +1,20 @@
+require 'amqp'
+
+EventMachine.run do
+  connection = AMQP.connect(:host => '127.0.0.1')
+  puts "Connecting to AMQP broker. Running #{AMQP::VERSION} version of the gem..."
+
+  channel  = AMQP::Channel.new(connection)
+  queue    = channel.queue("amqpgem.examples.hello_world", :auto_delete => true)
+  exchange = channel.default_exchange
+
+  queue.subscribe do |payload|
+    puts "Received a message: #{payload}. Disconnecting..."
+
+    connection.close {
+      EventMachine.stop { exit }
+    }
+  end
+
+  exchange.publish "Hello, world!", :routing_key => queue.name
+end
